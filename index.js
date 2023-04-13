e592291566f3ab1a522d3ce4a50f4b8aac8a685a
User <lcsgoes210@gmail.com>
2023-04-13 16:30:20 -0300

    commit

--- index.js
+++
@@ -1,6 +0,0 @@
-document.getElementById("audio").addEventListener("pause", function() {
-    document.getElementById("omori").src = "images/omori.png";
-});
-document.getElementById("audio").addEventListener("play", function() {
-    document.getElementById("omori").src = "images/omori.gif";
-});
